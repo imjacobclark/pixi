a2e0c45c294f9c398f59ec4de98de9a576cb5978
Jacob Clark <jacob.clark@bbc.co.uk>
2019-10-07 22:28:33 +0100

    Running squirrel!

---
+++ SquirrelMan/src/frames/player/idle.js
@@ -0,0 +1,6 @@
+export default [
+    "images/png/sprites/player/idle/player-idle-1.png",
+    "images/png/sprites/player/idle/player-idle-2.png",
+    "images/png/sprites/player/idle/player-idle-3.png",
+    "images/png/sprites/player/idle/player-idle-4.png"
+];
---
+++ SquirrelMan/src/frames/player/running.js
@@ -0,0 +1,8 @@
+export default [
+    "images/png/sprites/player/run/player-run-1.png",
+    "images/png/sprites/player/run/player-run-2.png",
+    "images/png/sprites/player/run/player-run-3.png",
+    "images/png/sprites/player/run/player-run-4.png",
+    "images/png/sprites/player/run/player-run-5.png",
+    "images/png/sprites/player/run/player-run-6.png"
+];
